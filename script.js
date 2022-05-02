9ad3ad3c8f5d8424a856b07a6abd6892326ce9b9
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-02 13:52:14 +0200

    watch vue project

--- script.js
+++ script.js
@@ -11,21 +11,36 @@ const app = new Vue({
     },
 
     methods: {
-        
-
-
-        timeInterval(){
-             setInterval(()=>{
-                const time = new Date();
+        getTime(){
+            const time = new Date();
                 let h = time.getHours()
                 let m = time.getMinutes()
                 let s = time.getSeconds()
-                this.watchTime = 
-                `${(h < 10)? "0" + h: m} : ${(m < 10)? "0" + m: m} : ${(s < 10)? "0" + s : s}`
+                const theRealtime = `${(h < 10)? "0" + h: m} : ${(m < 10)? "0" + m: m} : ${(s < 10)? "0" + s : s}`
+                return theRealtime
+        },
+
+        timeInterval(){
+            this.watchRealtime = setInterval(()=>{
+                
+                this.watchTime = this.getTime()
+                
             },1000)
         },
         watch(){
+            this.watchTime = this.getTime()
+            if(this.showTime === false){
+                this.showTime = true
+                this.btnMessage = "Hide Watch"
                 this.timeInterval()
+            }else{
+                this.showTime = false
+                clearInterval(this.watchRealtime)
+                this.watchTime = ""
+                this.btnMessage = "show time"
+
+            }
+                
 
            
         }
