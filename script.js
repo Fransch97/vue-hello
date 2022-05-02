fa8b729a68b01e9274ad3cdb5f11cfc9de0b846f
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-02 13:41:59 +0200

    my tests

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ const app = new Vue({
     el: '#app',
 
     data :{
-        message :"Hello Vue.js"
-    }
+        message :"Hello Vue.js",
+        btnMessage: "show time",
+        showTime: false,
+        watchTime: ""
+    },
+
+    methods: {
+        
+
+
+        timeInterval(){
+             setInterval(()=>{
+                const time = new Date();
+                let h = time.getHours()
+                let m = time.getMinutes()
+                let s = time.getSeconds()
+                this.watchTime = 
+                `${(h < 10)? "0" + h: m} : ${(m < 10)? "0" + m: m} : ${(s < 10)? "0" + s : s}`
+            },1000)
+        },
+        watch(){
+                this.timeInterval()
+
+           
+        }
+    },
 }
 )
